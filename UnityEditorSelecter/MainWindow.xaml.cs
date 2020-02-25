a58417b2b3216848792715f5ddf1e73e0a512a24
kamonohasikamo <arrowsuke@yahoo.co.jp>
2020-02-26 00:16:53 +0900

    ScrollViewerでチェックボックスの画像を表示させれた

--- UnityEditorSelecter/MainWindow.xaml.cs
+++ UnityEditorSelecter/MainWindow.xaml.cs
@@ -12,6 +12,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+public class PrefList
+{
+    public ObservableCollection<string> Data { get; }
+    public PrefList()
+    {
+        Data = new ObservableCollection<string>();
+        Data.Add("北海道");
+        Data.Add("青森県");
+        Data.Add("岩手県");
+        Data.Add("秋田県");
+    }
+}
 
 namespace UnityEditorSelecter
 {
@@ -23,6 +37,8 @@ namespace UnityEditorSelecter
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new PrefList();
         }
     }
 }
---
+++ UnityEditorSelecter/PrefList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+public class PrefList
+{
+    public ObservableCollection<string> Data { get; }
+    public PrefList()
+    {
+        Data = new ObservableCollection<string>();
+        Data.Add("北海道");
+        Data.Add("青森県");
+        Data.Add("岩手県");
+        Data.Add("秋田県");
+    }
+}
